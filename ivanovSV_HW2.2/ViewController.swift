658fee270fea05e9680a3da4367a0cca87abbf96
Sergey Ivanov <ggdlin@gmail.com>
2020-10-17 23:26:02 +0300

    Initial Commit

---
+++ ivanovSV_HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ivanovSV_HW2.2
+//
+//  Created by Admin on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
