b90fc4c2dc3017b2564909f29b2502d768cc53d3
Sergey Ivanov <ggdlin@gmail.com>
2020-10-18 02:09:14 +0300

    final version HW2.2

--- ivanovSV_HW2.2/ViewController.swift
+++ ivanovSV_HW2.2/ViewController.swift
@@ -21,14 +21,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mixedRGBView.layer.cornerRadius = 20
+        setupSliders()
+        setupValueLabels()
+        mixColors()
     }
 
     @IBAction func redSliderAction() {
+        mixColors()
+        setupValueLabels()
     }
     @IBAction func greenSliderAction() {
+        mixColors()
+        setupValueLabels()
     }
     @IBAction func blueSliderAction() {
+        mixColors()
+        setupValueLabels()
+    }
+    
+    private func mixColors() {
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        
+        mixedRGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    private func setupSliders() {
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    private func setupValueLabels() {
+        let redColorValue = String(redSlider.value)
+        let greenColorValue = String(greenSlider.value)
+        let blueColorValue = String(blueSlider.value)
+        
+        redLabel.text = String(redColorValue.prefix(4))
+        greenLabel.text = String(greenColorValue.prefix(4))
+        blueLabel.text = String(blueColorValue.prefix(4))
+        
     }
     
 }
